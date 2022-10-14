4a47a0f29c975129e6be9b8152b1f8af5137b98c
zyy2003 <62226863+zyy2003@users.noreply.github.com>
2022-10-14 13:34:41 +0800

    Add files via upload

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor v-for="floor in floorList" :key="floor.id" :list="floor"></Floor>
+    <Brand></Brand>
+  </div>
+</template>
+
+<script>
+//非路由组件在使用的时候分为三大步：定义、注册、使用
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+import {mapState} from 'vuex'
+
+export default {
+  name: "",
+  components: {ListContainer, Recommend, Rank, Like, Floor, Brand},
+  mounted() {
+    //派发action,获取floor组件的数据
+    this.$store.dispatch("getFloorList");
+    this.$store.dispatch('getUserInfo');
+  },
+  computed: {
+    ...mapState({
+      floorList: (state)=>state.home.floorList
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
